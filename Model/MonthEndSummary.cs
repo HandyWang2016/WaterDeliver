b14993069c89534e8be5807abc771fdc9f074c52
wanghongyuan <baweier2016@163.com>
2018-01-08 14:23:54 +0800

    添加月底收支汇总；修改记录/汇总页面收入支出金额的样式

---
+++ Model/MonthEndSummary.cs
@@ -0,0 +1,33 @@
+﻿namespace Model
+{
+    /// <summary>
+    /// 月底账目汇总实体
+    /// </summary>
+    public class MonthEndSummary
+    {
+        /// <summary>
+        /// 员工总收入
+        /// </summary>
+        public double StaffEarn { get; set; }
+        /// <summary>
+        /// 员工总支出
+        /// </summary>
+        public double StaffPay { get; set; }
+        /// <summary>
+        /// 公司总收入
+        /// </summary>
+        public double CompanyEarn { get; set; }
+        /// <summary>
+        /// 公司总支出
+        /// </summary>
+        public double CompanyPay { get; set; }
+        /// <summary>
+        /// 月底总盈利：员工收入+公司收入-员工支出-公司支出
+        /// </summary>
+        public double MonthEndEarn { get; set; }
+        /// <summary>
+        /// 结算的年份月份
+        /// </summary>
+        public string YearMonth { get; set; }
+    }
+}
---
+++ WaterDeliver/Controllers/SummaryController.cs
@@ -0,0 +1,70 @@
+﻿using Common.BusinessHelper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Model;
+using Newtonsoft.Json;
+
+namespace WaterDeliver.Controllers
+{
+    public class SummaryController : BaseController
+    {
+        // GET: Summary
+        public ActionResult Index(string yearMonth)
+        {
+            int year;
+            int month;
+
+            if (string.IsNullOrEmpty(yearMonth))
+            {
+                //默认显示上一个月的月底结算情况
+                year = DateTime.Now.AddMonths(-1).Year;
+                month = DateTime.Now.AddMonths(-1).Month;
+                yearMonth = year + "-" + month;
+            }
+            else
+            {
+                year = int.Parse(yearMonth.Split('-')[0]);
+                month = int.Parse(yearMonth.Split('-')[1]);
+            }
+
+            var dailyRecords = DailyRecordHelper.DailyRecordList().Where(item => item.VisitDate.Year == year && item.VisitDate.Month == month);
+            var companyRecords = CompanyRecordHelper.CompanyList().Where(item => item.TransTime.Year == year && item.TransTime.Month == month);
+            MonthEndSummary monthEnd = new MonthEndSummary();
+            //员工汇总
+            MonthEndSummary monthEnd1 = dailyRecords.GroupBy(item => new { item.VisitDate.Year, item.VisitDate.Month })
+                .Select(g => new MonthEndSummary()
+                {
+                    StaffEarn = g.Sum(x => x.EarnMonthEndPrice) + g.Sum(x => x.EarnDeposit),
+                    StaffPay = g.Sum(x => x.PayDeposit)
+
+                }).FirstOrDefault();
+
+            //公司汇总
+            MonthEndSummary monthEnd2 = companyRecords.GroupBy(item => new { item.TransTime.Year, item.TransTime.Month })
+                .Select(g => new MonthEndSummary()
+                {
+                    CompanyEarn = g.Where(item => item.IsPayType == false).Sum(x => x.TransSum),
+                    CompanyPay = g.Where(item => item.IsPayType == true).Sum(x => x.TransSum)
+                }).FirstOrDefault();
+
+            monthEnd.StaffEarn = monthEnd1?.StaffEarn ?? 0;
+            monthEnd.StaffPay = monthEnd1?.StaffPay ?? 0;
+            monthEnd.CompanyEarn = monthEnd2?.CompanyEarn ?? 0;
+            monthEnd.CompanyPay = monthEnd2?.CompanyPay ?? 0;
+            monthEnd.MonthEndEarn = monthEnd.StaffEarn + monthEnd.CompanyEarn - monthEnd.StaffPay - monthEnd.CompanyPay;
+            monthEnd.YearMonth = year + "-" + month;
+
+            ViewBag.queryPam = JsonConvert.SerializeObject(new { YearMonth = yearMonth });
+            ViewBag.flag = "EndSummary";
+            return View(monthEnd);
+        }
+
+        public ActionResult QueryMonthEnd(string yearMonth)
+        {
+            return RedirectToAction("Index");
+        }
+    }
+}
