20c89843213542e59c0e5187761f54f866d9de84
wanghongyuan <baweier2016@163.com>
2018-01-10 17:04:54 +0800

    水厂交易汇总页面功能开发

--- Model/Factory.cs
+++ Model/Factory.cs
@@ -20,4 +20,46 @@ namespace Model
         /// </summary>
         public string Describe { get; set; }
     }
+
+    /// <summary>
+    /// 水厂月库存统计
+    /// </summary>
+    public class FactoryStock
+    {
+        public string FactoryName { get; set; }
+        public string ProductName { get; set; }
+        public int BucketStock { get; set; }
+        public int EmptyBucketStock { get; set; }
+        public string TransMonth { get; set; }
+    }
+
+    public class FactoryTrans
+    {
+        /// <summary>
+        /// 水厂名称
+        /// </summary>
+        public string FactoryName { get; set; }
+        /// <summary>
+        /// 涉及水厂的交易类别
+        /// </summary>
+        public string TransType { get; set; }
+        /// <summary>
+        /// 交易金额
+        /// </summary>
+        public double TransSum { get; set; }
+        /// <summary>
+        /// 是否支出类型
+        /// </summary>
+        public bool IsPayType { get; set; }
+        /// <summary>
+        /// 交易月份 eg:2017-12
+        /// </summary>
+        public string TransMonth { get; set; }
+    }
+
+    public class FactorySumaryViewModel
+    {
+        public List<FactoryStock> FactoryStock { get; set; }
+        public List<FactoryTrans> FactoryTrans { get; set; }
+    }
 }
--- WaterDeliver/Controllers/Home_FactoryController.cs
+++ WaterDeliver/Controllers/Home_FactoryController.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Common.BusinessHelper;
+using Model;
+using Newtonsoft.Json;
 
 namespace WaterDeliver.Controllers
 {
@@ -14,14 +16,80 @@ namespace WaterDeliver.Controllers
         /// 2. 水厂交易明细 支付总押金 退还总押金 进水总支出
         /// </summary>
         /// <returns></returns>
-        public ActionResult TransRecord()
+        public ActionResult TransRecord(string yearMonth, string factoryId)
         {
+            int year;
+            int month;
+            if (string.IsNullOrEmpty(yearMonth))
+            {
+                //默认显示上一个月的月底结算情况
+                year = DateTime.Now.AddMonths(-1).Year;
+                month = DateTime.Now.AddMonths(-1).Month;
+                yearMonth = year + "-" + month;
+            }
+            else
+            {
+                year = int.Parse(yearMonth.Split('-')[0]);
+                month = int.Parse(yearMonth.Split('-')[1]);
+            }
             var factories = FactoryHelper.FactoryList();
             var products = ProductHelper.ProductList();
+            var payTypes = CompanyPayTypeHelper.PayTypeList();
+            //选定年月下的日常、公司交易记录
             var companyRecords = CompanyRecordHelper.CompanyList();
+            var dailyRecords = DailyRecordHelper.DailyRecordList();
+            //1.库存明细
+            var factoryStocks = (from r in dailyRecords
+                                 join p in products
+                                 on r.SendProductId equals p.Id
+                                 where r.VisitDate.Year == year && r.VisitDate.Month == month
+                                 join f in factories
+                                 on p.FactoryId equals f.Id
+                                 where f.Id == (string.IsNullOrEmpty(factoryId) ? f.Id : factoryId)
+                                 group
+                                 new { p.FactoryId, p.Id, r.SendBucketAmount, r.ReceiveEmptyBucketAmount, p.ProductName, f.FactoryName } by
+                                 new { p.FactoryId, p.Id }
+                into t
+                                 select new FactoryStock
+                                 {
+                                     FactoryName = t.First().FactoryName,
+                                     TransMonth = yearMonth,
+                                     ProductName = t.First().ProductName,
+                                     BucketStock = t.Sum(i => i.SendBucketAmount),
+                                     EmptyBucketStock = t.Sum(i => i.ReceiveEmptyBucketAmount)
+                                 }).OrderByDescending(item => item.FactoryName);
 
+            //2.交易明细
+            var factoryTrans = (from c in companyRecords
+                                join f in factories
+                                on c.FactoryId equals f.Id
+                                where c.TransTime.Year == year && c.TransTime.Month == month && f.Id == (string.IsNullOrEmpty(factoryId) ? f.Id : factoryId)
+                                join p in payTypes
+                                on c.PayTypeId equals p.Id
+                                group new { c.IsPayType, c.TransSum, f.FactoryName, p.PayType } by
+                                new { c.FactoryId, c.PayTypeId }
+                into t
+                                select new FactoryTrans
+                                {
+                                    FactoryName = t.First().FactoryName,
+                                    TransType = t.First().PayType,
+                                    IsPayType = t.First().IsPayType,
+                                    TransSum = t.Sum(i => i.TransSum),
+                                    TransMonth = yearMonth
+                                }).OrderByDescending(item => item.FactoryName);
+
+            FactorySumaryViewModel factorySumary = new FactorySumaryViewModel
+            {
+                FactoryStock = factoryStocks.ToList(),
+                FactoryTrans = factoryTrans.ToList()
+            };
+
+            ViewBag.queryPam = JsonConvert.SerializeObject(new { YearMonth = yearMonth, FactoryId = factoryId });
+            factories.Insert(0, new Factory { Id = "", FactoryName = "" });
+            ViewBag.factories = factories;
             ViewBag.flag = "FactoryTransRecord";
-            return View();
+            ViewBag.YearMonth = yearMonth;
+            return View(factorySumary);
         }
     }
 }
