b79356dbcf015a38fdb468cdc75cd3a475ad1ad1
HandyT410 <baweier2010@163.com>
2018-01-20 17:53:10 +0800

    完成公司附属产品库存展示

---
+++ Model/CustomerAccessory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class CustomerAccessory
+    {
+        public string CustomerId { get; set; }
+        public string CustomerName { get; set; }
+        public int WaterDispenser { get; set; }
+        public int WaterHolder { get; set; }
+        public int PushPump { get; set; }
+    }
+}
--- WaterDeliver/Controllers/Home_AccessoryController.cs
+++ WaterDeliver/Controllers/Home_AccessoryController.cs
@@ -1,4 +1,5 @@
 ﻿using Common.BusinessHelper;
+using Model;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,8 +17,26 @@ namespace WaterDeliver.Controllers
         public ActionResult Index()
         {
             var records = DailyRecordHelper.DailyRecordList();
+            var customers = CustomerHelper.CustomerList();
 
-            return View();
+            var recordsAccessory = records.GroupBy(item => item.CustomerId)
+                .Select(t => new CustomerAccessory
+                {
+                    CustomerId = t.First().CustomerId,
+                    WaterDispenser = t.Sum(i => i.WaterDispenser),
+                    WaterHolder = t.Sum(i => i.WaterHolder),
+                    PushPump = t.Sum(i => i.PushPump)
+                }).Join(customers, x => x.CustomerId, y => y.Id, (x, y) => new { x, y }).Select(p => new CustomerAccessory
+                {
+                    CustomerName = p.y.CustomerName,
+                    WaterDispenser = p.x.WaterDispenser,
+                    WaterHolder = p.x.WaterHolder,
+                    PushPump = p.x.PushPump
+                }).ToList();
+            ViewBag.flag = "CustomerAccessory";
+            return View(recordsAccessory);
         }
     }
+
+    
 }
